8f5054df5c0d8aafdf9ebda98f904aed5b3a8d9a
merri <merave24@evergreen.edu>
2021-03-13 13:05:57 -0800

    deleted unnecessary class

--- finalprojectwinter/src/finalprojectwinter/GUI.java
+++ finalprojectwinter/src/finalprojectwinter/GUI.java
@@ -159,7 +159,8 @@ public class GUI {
 				}
 		);
 		
-		//FINISH
+		/*load functionality. choose a file of .txt or .ser form (possibly others but this program saves txts so what can ya do) in a very ugly
+		 * file chooser and loads it in. it doesn't like reading "object" into an "arraylist<sortalgorithms> but it has all the data*/
 		JButton loadArrays = new JButton("Load");
 		loadArrays.addActionListener(
 				new ActionListener() {
@@ -185,7 +186,6 @@ public class GUI {
 							
 							for (int i = 1; i <= last; ++i) {
 								loadTracker.get(i).setN();
-								System.out.println(loadTracker.get(i).getN());
 							}
 							
 							for (int i = 1; i <= last; ++i) {
